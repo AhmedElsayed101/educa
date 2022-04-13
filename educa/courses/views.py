29fee08e749e24cadcead9596325288833b03a91
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-04-13 10:25:50 +0200

    Create courses list and delete views

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Course
 
-# Create your views here.
+
+
+class OwnerMixin(object):
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin(object):
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    tempalte_name = 'courses/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    tempalte_name = 'courses/manage/course/list.html'
+
+
+class CourseDeleteView(OwnerCourseEditMixin, DeleteView):
+    tempalte_name = 'courses/manage/course/delete.html'
+    
