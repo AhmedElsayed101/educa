b84bc0c1596efa5b4ba0884dd08f8430078df7c4
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-04-19 01:53:07 +0200

    Create content serializer

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,6 +1,6 @@
 from ast import Sub
 from rest_framework import serializers
-from ..models import Subject, Course, Module
+from ..models import Subject, Course, Module, Content
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -19,8 +19,38 @@ class ModuleSerializer(serializers.ModelSerializer):
 class CourseSerializer(serializers.ModelSerializer):
 
     modules = ModuleSerializer(many=True, read_only=True)
+
     class Meta:
         model = Course
-        fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
+        fields = ['id', 'subject', 'title', 'slug',
+                  'overview', 'created', 'owner', 'modules']
 
 
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fileds = ['order', 'item']
+
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description', 'contents']
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug',
+                  'overview', 'created', 'owner', 'modules']
