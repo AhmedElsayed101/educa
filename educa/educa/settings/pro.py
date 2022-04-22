068e6e37c58164595c3a2f3667faa63ec238f830
ahmedelsayed101 <ahmedessayed101@gmail.com>
2022-04-22 06:43:59 +0200

    Add DB settings and a secret key

--- educa/educa/settings/pro.py
+++ educa/educa/settings/pro.py
@@ -2,6 +2,8 @@ from .base import *
 
 DEBUG = False
 
+SECRET_KEY = 'secret'
+
 ADMINS = (
     ('Ahmed', 'ahmedelee@gmail.com'),
 )
@@ -10,5 +12,9 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'educa',
+        'USER': 'educa',
+        'PASSWORD': 'educa', # should use env vars.
     }
 }
